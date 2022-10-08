c4865e28fd919e09f42a5b48b854acd827b006f3
elluis <elluis.codes@gmail.com>
2022-10-08 18:27:39 -0500

    setup fallback: 'blocking' - allows loading jit serving of none ssr pages - without having to setup 'Loading...' behaviour -
    setup dynamic generation of [pid] values - this maps all dynamic paths in dummy-data -

--- pages/[pid].js
+++ pages/[pid].js
@@ -7,36 +7,34 @@ export default function ProductDetailPage (props) {
     const { loadedProduct } = props;
 
     if (!loadedProduct) {
-        return (
-            <p>Loading...</p>
-        )
+        return (<p>Loading...</p>)
     }
 
     return (<div className={ styles.container }>
-            <div className={ styles.main }>
-                <h1>{ loadedProduct.title }</h1>
-                <p>{ loadedProduct.description }</p>
-            </div>
+        <div className={ styles.main }>
+            <h1>{ loadedProduct.title }</h1>
+            <p>{ loadedProduct.description }</p>
         </div>
-    )
+    </div>)
 }
 
+
+async function getData () {
+    const filePath = path.join (process.cwd (), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile (filePath);
+    const data = JSON.parse (jsonData);
+
+    return data;
+}
 
 // context can be used
 export async function getStaticProps (context) {
     // params holds a dictionary of key:value pairs
     const { params } = context;
-
     const productId = params.pid;
-
     console.log (productId);
-
-    const filePath = path.join (process.cwd (), 'data', 'dummy-backend.json');
-    const jsonData = await fs.readFile (filePath);
-    const data = JSON.parse (jsonData);
-
+    const data = await getData ();
     const product = data.products.find (product => product.id === productId);
-
     return {
         props: {
             loadedProduct: product
@@ -47,18 +45,24 @@ export async function getStaticProps (context) {
 // tells next.js what productId should be generated
 export async function getStaticPaths () {
 
+    const data = await getData();
+    const ids = data.products.map((product) => product.id);
+    const pathWithParams = ids.map(id => ({params: {pid: id}}))
+
+
     return {
-        paths: [
-            { params: { pid: 'p1' } },
-            { params: { pid: 'p2' } },
-            // { params: { pid: 'p3' } },
+        paths: pathWithParams,
+
+        // paths: [ { params: { pid: 'p1' } }, { params: { pid: 'p2' } }, // { params: { pid: 'p3' } },
             // { params: { pid: 'p4' } },
-            // { params: { pid: 'p5' } },
-        ],
+            // { params: { pid: 'p5' } },]
         // fallback: true - will only pre-generate pages specified in paths: yet will allow us to access other pages not defined
         // within paths: by making calls to the data just in time -
         // in this example p1 && p2 are pre-rendered // calls to p3,p4,p5 are not generated until they are called upon
-        fallback: true
+        // fallback: true
+        // setting fallback: 'blacking' allows us to serve pages without a 'loading...' screen -
+        fallback: 'blocking'
+
     }
 
 }
