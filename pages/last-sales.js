afd13a8e6766fa182be006f63cc0e27de1396b34
elluis <elluis.codes@gmail.com>
2022-10-08 22:01:47 -0500

    implemented client side data fetching with useSWR() via :: https://swr.vercel.app/ - commented out default useEffect(fetch.then.then) pattern -

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,41 +1,65 @@
 import { useEffect, useState } from "react";
+import useSWR from 'swr';
 
-export default function LastSalesPage ()
-{
+export default function LastSalesPage () {
 
     const [ sales, setSales ] = useState ();
-    const [ isLoading, setIsLoading ] = useState (false);
+    const { data, error } = useSWR ('https://elluis-nextjs-practic-default-rtdb.firebaseio.com/sales.json', (url)=> fetch(url).then(res =>res.json()));
 
-    useEffect (() => {
-        setIsLoading (true);
 
-        fetch ('https://elluis-nextjs-practic-default-rtdb.firebaseio.com/sales.json')
-            .then ((response) => response.json ())
-            .then (data => {
-            const transformedSales = [];
-            for (const key in data) {
-                transformedSales.push ({ id: key, username: data[key].username, volume: data[key].volume })
+    useEffect (
+        () => {
+            if (data) {
+                const transformedSales = [];
+                for (const key in data) {
+                    transformedSales.push ({
+                        id: key,
+                        username: data[key].username,
+                        volume: data[key].volume,
+                    })
+                }
+                setSales (transformedSales);
             }
+        }, [ data ])
 
-            setSales (transformedSales);
-            setIsLoading (false);
-
-        })
-    }, [])
-
-    if (isLoading) {
+    if (error) {
+        return <p>Error loading...</p>
+    }
+    if (!data || !sales) {
         return <p>Loading...</p>;
     }
-    if (!sales) {
-        return <p>-no data yet dude</p>;
-    }
+
+    //--- working version of common useEffect() data fetching hook below ---//
+    // all commented statements in block below are components of this pattern
+
+    // const [ sales, setSales ] = useState ();
+    // const [ isLoading, setIsLoading ] = useState (false);
+    // useEffect (() => {
+    //     setIsLoading (true);
+    //     fetch ('https://elluis-nextjs-practic-default-rtdb.firebaseio.com/sales.json')
+    //         .then ((response) => response.json ())
+    //         .then (data => {
+    //             const transformedSales = [];
+    //             for (const key in data) {
+    //                 transformedSales.push ({ id: key, username: data[key].username, volume: data[key].volume })
+    //             }
+    //
+    //             setSales (transformedSales);
+    //             setIsLoading (false);
+    //
+    //         })
+    // }, [])
+    // if (isLoading) {
+    //     return <p>Loading...</p>;
+    // }
+    // if (!sales) {
+    //     return <p>-no data yet dude</p>;
+    // }
 
 
-    return (
-        <ul>
+    return (<ul>
         { sales.map ((sale) => {
             return (<li key={ sale.id }>{ sale.username } - ${ sale.volume }</li>)
         }) }
-    </ul>
-    );
+    </ul>);
 }
